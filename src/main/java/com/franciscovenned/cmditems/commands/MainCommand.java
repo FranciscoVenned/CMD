fe1e12c3f2d710afa9bda8842b1cebf4d9f13c5c
FranciscoVenned <Jorge_Francisco_Ayala@hotmail.com>
2020-11-11 00:04:30 -0500

    Arreglada vulnerabilidad metodo send. (Posible exploit)

--- src/main/java/com/franciscovenned/cmditems/commands/MainCommand.java
+++ src/main/java/com/franciscovenned/cmditems/commands/MainCommand.java
@@ -19,13 +19,13 @@ import java.util.List;
 
 public class MainCommand implements CommandExecutor {
     final private CMDItems plugin;
-    private CommandSender sender;
+//    private CommandSender sender;
 
     public MainCommand(CMDItems plugin) {
         this.plugin = plugin;
     }
 
-    private void send(String message) {
+    private void send(CommandSender sender,String message) {
         FileConfiguration config = plugin.getConfig();
         // LLamando al prefix establecido en el plugin.yml
         String prefix = config.getString("prefix");
@@ -101,19 +101,20 @@ public class MainCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        this.sender = sender;
+//        this.sender = sender;  (Esto puede perjudicar si hay muchas personas usando dicho comandos)
+
         FileConfiguration config = plugin.getConfig();
         if (args.length == 0 || args[0].equalsIgnoreCase("help")) {
-            send("&6Lista de comandos");
-            send("&f/" + label + " help");
-            send("&f/" + label + " version");
-            send("&f/" + label + " reload");
-            send("&f/" + label + " give (player) (tipo) (nivel)");
-            send("&f/" + label + " giveFuel (player) (tipo) <cantidad>");
-            send("&f/" + label + " editDrops");
-            send("&f/" + label + " list (player/tipo)");
-            send("&f/" + label + " shop");
-            send("&f/" + label + " toggleAvisos");
+            send(sender,"&6Lista de comandos");
+            send(sender,"&f/" + label + " help");
+            send(sender,"&f/" + label + " version");
+            send(sender,"&f/" + label + " reload");
+            send(sender,"&f/" + label + " give (player) (tipo) (nivel)");
+            send(sender,"&f/" + label + " giveFuel (player) (tipo) <cantidad>");
+            send(sender,"&f/" + label + " editDrops");
+            send(sender,"&f/" + label + " list (player/tipo)");
+            send(sender,"&f/" + label + " shop");
+            send(sender,"&f/" + label + " toggleAvisos");
             return true;
         }
         switch (args[0]) {
@@ -122,70 +123,70 @@ public class MainCommand implements CommandExecutor {
             //Comando para verificar version del plugin
 //    } else if (args[0].equalsIgnoreCase("version")) {
             case "version":
-                send("version: " + plugin.getVersion());
+                send(sender,"version: " + plugin.getVersion());
                 break;
 
             //Comando para la tienda del servidor
 //    } else if (args[0].equalsIgnoreCase("shop")) {
             case "shop":
                 if (sender instanceof ConsoleCommandSender) {
-                    send("No puedes abrir la shop desde la consola pelotudo");
+                    send(sender,"No puedes abrir la shop desde la consola pelotudo");
 
                     return true; //No sigue leyendo las lineas siguientes.
                 }   // "!" signo de negacion osea si el sender no tiene permisos
                 if (!sender.hasPermission("cmditems.shop")) {
-                    send("No tienes permisos down");
+                    send(sender,"No tienes permisos down");
                     return true; //obligatorio el return por que sino pasara por encima
 
                 }
                 //Si la cantidad de argumentos es menor a se ejecuta esta linea
                 if (args.length < 2) {
-                    send("necesitas mas argumentos");
+                    send(sender,"necesitas mas argumentos");
 
                     return true;
                 }
                 Player player2 = Bukkit.getPlayer(args[1]);
                 // /m shop pancho
                 if (player2 == null) {
-                    send("Ese jugador no existe o no esta conectado");
+                    send(sender,"Ese jugador no existe o no esta conectado");
                     return true;
                 }
                 //Player = El tipo que le queremos ofrecer al metodo "castear"
                 Player player = (Player) sender;
-                send("Shop abierto para " + player2.getName());
+                send(sender,"Shop abierto para " + player2.getName());
                 break;
 
             case "give":
                 if (sender instanceof ConsoleCommandSender) {
-                    send("No puedes enviar objetos desde la consola");
+                    send(sender,"No puedes enviar objetos desde la consola");
                     return true;
                 }
                 if (!sender.hasPermission("cmditems.give")) {
-                    send("No tienes permisos");
+                    send(sender,"No tienes permisos");
                     return true;
                 }
                 if (args.length < 2) {
-                    send("Necesitas colocar al jugador");
+                    send(sender,"Necesitas colocar al jugador");
                     return true;
                 }
                 Player player3 = Bukkit.getPlayer(args[1]);
                 if (player3 == null) {
-                    send("Ese jugandor no esta conectado");
+                    send(sender,"Ese jugandor no esta conectado");
                     return true;
                 }
                 if (args.length < 3) {
-                    send("Coloca el id del bloque");
+                    send(sender,"Coloca el id del bloque");
                     return true;
                 }
                 if (args.length < 4) {
-                    send("Coloca la cantidad");
+                    send(sender,"Coloca la cantidad");
                     return true;
                 }
                 Material tipodeitem;
                 try {  //Es para evitar que salga un error en la consola y controlar cuando va haber un error en el complemento.
                     tipodeitem = Material.valueOf(args[2]);
                 } catch (Exception exception) {
-                    send("Ese material no es valido");
+                    send(sender,"Ese material no es valido");
                     return true;
 
                 }
@@ -193,7 +194,7 @@ public class MainCommand implements CommandExecutor {
                 try {
                     cantidad = Integer.parseInt(args[3]);
                 } catch (NumberFormatException exception) {
-                    send("Ese no es un numero valido");
+                    send(sender,"Ese no es un numero valido");
                     return true;
                 }
                 ItemStack itemStack = new ItemStack(tipodeitem, cantidad); // Dos valores establecidos en el constructor
@@ -201,28 +202,28 @@ public class MainCommand implements CommandExecutor {
                 break;
 
             case "stop":
-                send("server apagado");
+                send(sender,"server apagado");
                 plugin.getServer().shutdown();
                 break;
             // default como un else
             case "prueba":
                 if (args.length < 2) {
-                    send("requiere mas argumentos");
+                    send(sender,"requiere mas argumentos");
                     return true;
                 }
                 // Una lista de Strings de camino a la config.yml
                 if (config.contains(args[1])) {
-                    send(config.getString(args[1]));
+                    send(sender,config.getString(args[1]));
                     return true;
                 }
                 break;
             case "get":
                 if (!sender.hasPermission("cmditems.get")) {
-                    send("No tienes permisos");
+                    send(sender,"No tienes permisos");
                     return true;
                 }
                 if (sender instanceof ConsoleCommandSender) {  //Se fija en la Consola
-                    send("No se puede enviar ese comando desde la consola");
+                    send(sender,"No se puede enviar ese comando desde la consola");
                     return true;
                 }
                 Player player4 = (Player) sender; //Transformar el sender en un player
@@ -240,15 +241,15 @@ public class MainCommand implements CommandExecutor {
                 meta.setLore(lorecolores); //Establecer la nueva lista creada por Arraylist
                 item.setItemMeta(meta);
                 player4.getInventory().addItem(item);
-                send("Has obtenido el item con exito");
+                send(sender,"Has obtenido el item con exito");
                 break;
             case "espada":
                 if (!sender.hasPermission("cmditems.espada")) {
-                    send("No tienes permisos para la espada");
+                    send(sender,"No tienes permisos para la espada");
                     return true;
                 }
                 if (sender instanceof ConsoleCommandSender){
-                    send("No puedes ejecutar el siguiente comando en la consola");
+                    send(sender,"No puedes ejecutar el siguiente comando en la consola");
                     return true;
                 }
                 Player player5 = (Player) sender;
@@ -266,10 +267,10 @@ public class MainCommand implements CommandExecutor {
                 meta2.setLore(lorecolores2);
                 item2.setItemMeta(meta2);
                 player5.getInventory().addItem(item2);
-                send("Obtuviste el item con exito");
+                send(sender,"Obtuviste el item con exito");
                 break;
             default:
-                send("comando desconocido");
+                send(sender,"comando desconocido");
 
 
         }
